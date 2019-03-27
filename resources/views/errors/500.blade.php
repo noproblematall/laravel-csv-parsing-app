713e8c3516664d6382866ee1240fa661c8a03021
David Blackwell <david1213117@gmail.com>
2019-03-28 00:43:15 +0800

    Complete 4th milestone, only remained admin page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.error')
+@section('styles')
+
+@endsection
+
+@section('content')
+<section class="probootstrap-section" id="working-area" data-section="working-area">
+    <h1 class="text-center">500 Server Error</h1>
+</section>
+@endsection
+
+@section('scripts')
+
+@endsection
